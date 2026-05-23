7def43ff89896202db3b465a9110ac0cdef89c3f
zhenyan121 <3367366583@qq.com>
2026-05-23 13:29:06 +0800

    perf: improve river naturalness

--- src/gameplay/river_worm.cpp
+++ src/gameplay/river_worm.cpp
@@ -33,7 +33,7 @@ void RiverWorm::try_to_add_path(const ChunkPos& chunk_pos,
         const int CHUNK_MAX_X = CHUNK_MIN_X + SIZE_X - 1;
         const int CHUNK_MAX_Z = CHUNK_MIN_Z + SIZE_Z - 1;
         int x = random.random_int(CHUNK_MIN_X, CHUNK_MAX_X);
-        int y = SEA_LEVEL;
+        int y = SEA_LEVEL + 2;
         int z = random.random_int(CHUNK_MIN_Z, CHUNK_MAX_Z);
         add_path(glm::vec3{x, y, z});
     }
