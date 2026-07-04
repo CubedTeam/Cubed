e80d848095abcee4ce5b63df6bcec6d0288041c8
zhenyan121 <3367366583@qq.com>
2026-07-04 18:05:37 +0800

    refactor(player_renderer): split rendering into per-body-part VAOs and VBOs
    Move `PlayerVertex` struct to header and replace single VAO/VBO with arrays of 6 parts. Implement head pitch rotation in `render()` and `shadow_render()` by applying a separate model matrix for the head (index 0). Remove old `vao()` and `sum()` accessors. Adjust model translation for consistency.

--- include/Cubed/player_renderer.hpp
+++ include/Cubed/player_renderer.hpp
@@ -11,14 +11,20 @@ public:
     void init();
     void render(const Shader& shader);
     void shadow_render(const Shader& shader, glm::mat4& light_matrix);
-    GLuint vao();
-    int sum();
 
 private:
+    static constexpr int BODY_PART_NUM = 6;
+    struct PlayerVertex {
+
+        float x = 0.0f, y = 0.0f, z = 0.0f;
+        float s = 0.0f, t = 0.0f;
+        float nx = 0.0f, ny = 0.0f, nz = 0.0f;
+        float tx = 0.0f, ty = 0.0f, tz = 0.0f;
+    };
     Renderer& m_renderer;
-    GLuint m_vao;
-    GLuint m_vbo;
-    int m_vertices_sum = 0;
+    std::array<GLuint, BODY_PART_NUM> m_vao;
+    std::array<GLuint, BODY_PART_NUM> m_vbo;
     bool m_inited{false};
+    std::array<std::vector<PlayerVertex>, BODY_PART_NUM> m_vertices;
 };
 } // namespace Cubed
--- src/player_renderer.cpp
+++ src/player_renderer.cpp
@@ -12,14 +12,6 @@ struct Cuboid {
     glm::vec3 size;
 };
 
-struct PlayerVertex {
-
-    float x = 0.0f, y = 0.0f, z = 0.0f;
-    float s = 0.0f, t = 0.0f;
-    float nx = 0.0f, ny = 0.0f, nz = 0.0f;
-    float tx = 0.0f, ty = 0.0f, tz = 0.0f;
-};
-
 constexpr Cuboid PLAYER_MODEL[] = {
     // Head
     {{0.25f, 1.50f, 0.25f}, {0.50f, 0.50f, 0.50f}},
@@ -48,13 +40,14 @@ PlayerRenderer::~PlayerRenderer() {
     if (!m_inited) {
         return;
     }
-    glDeleteBuffers(1, &m_vbo);
-    glDeleteVertexArrays(1, &m_vao);
+    glDeleteBuffers(BODY_PART_NUM, m_vbo.data());
+    glDeleteVertexArrays(BODY_PART_NUM, m_vao.data());
 }
 
 void PlayerRenderer::init() {
-    std::vector<PlayerVertex> vertices;
-    for (const auto& part : PLAYER_MODEL) {
+
+    for (int i = 0; i < 6; i++) {
+        const auto& part = PLAYER_MODEL[i];
         for (int face = 0; face < 6; ++face) {
             for (int v = 0; v < 6; ++v) {
                 glm::vec3 p{VERTICES_POS[face][v][0], VERTICES_POS[face][v][1],
@@ -62,38 +55,42 @@ void PlayerRenderer::init() {
 
                 p *= part.size;
                 p += part.offset;
-                vertices.emplace_back(p.x, p.y, p.z, TEX_COORDS[face][v][0],
-                                      TEX_COORDS[face][v][1],
-                                      NORMALS[face][v][0], NORMALS[face][v][1],
-                                      NORMALS[face][v][2], TANGENTS[face][v][0],
-                                      TANGENTS[face][v][1],
-                                      TANGENTS[face][v][2]);
+                m_vertices[i].emplace_back(
+                    p.x, p.y, p.z, TEX_COORDS[face][v][0],
+                    TEX_COORDS[face][v][1], NORMALS[face][v][0],
+                    NORMALS[face][v][1], NORMALS[face][v][2],
+                    TANGENTS[face][v][0], TANGENTS[face][v][1],
+                    TANGENTS[face][v][2]);
             }
         }
     }
-    glGenVertexArrays(1, &m_vao);
-    glBindVertexArray(m_vao);
-    glGenBuffers(1, &m_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(PlayerVertex),
-                 vertices.data(), GL_STATIC_DRAW);
+    glGenVertexArrays(BODY_PART_NUM, m_vao.data());
+    glGenBuffers(BODY_PART_NUM, m_vbo.data());
+    for (int i = 0; i < BODY_PART_NUM; i++) {
+        glBindVertexArray(m_vao[i]);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
-                          (void*)0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
-                          (void*)offsetof(PlayerVertex, s));
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
-                          (void*)offsetof(PlayerVertex, nx));
-    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
-                          (void*)offsetof(PlayerVertex, tx));
+        glBindBuffer(GL_ARRAY_BUFFER, m_vbo[i]);
+        glBufferData(GL_ARRAY_BUFFER,
+                     m_vertices[i].size() * sizeof(PlayerVertex),
+                     m_vertices[i].data(), GL_STATIC_DRAW);
 
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
-    glEnableVertexAttribArray(3);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    m_vertices_sum = vertices.size();
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
+                              (void*)0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
+                              (void*)offsetof(PlayerVertex, s));
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
+                              (void*)offsetof(PlayerVertex, nx));
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(PlayerVertex),
+                              (void*)offsetof(PlayerVertex, tx));
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+        glEnableVertexAttribArray(3);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
     m_inited = true;
 }
 
@@ -125,13 +122,32 @@ void PlayerRenderer::render(const Shader& shader) {
         model = glm::rotate(model, glm::radians(player.yaw),
                             glm::vec3(0.0f, 1.0f, 0.0f));
 
-        model = glm::translate(model, glm::vec3(-0.5f, 0.0f, -0.5f));
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
         m_mv_mat = m_v_mat * model;
 
-        shader.set_loc("mv_matrix", m_mv_mat);
-        glBindVertexArray(m_vao);
-        glEnable(GL_DEPTH_TEST);
-        glDrawArrays(GL_TRIANGLES, 0, m_vertices_sum);
+        for (int i = 0; i < BODY_PART_NUM; i++) {
+            if (i == 0) {
+                glm::mat4 head_model = model;
+
+                head_model =
+                    glm::translate(head_model, glm::vec3(0.5f, 1.5f, 0.5f));
+
+                head_model =
+                    glm::rotate(head_model, glm::radians(-player.pitch),
+                                glm::vec3(-1, 0, 0));
+
+                head_model =
+                    glm::translate(head_model, glm::vec3(-0.5f, -1.5f, -0.5f));
+
+                glm::mat4 head_mv = m_v_mat * head_model;
+                shader.set_loc("mv_matrix", head_mv);
+            } else {
+                shader.set_loc("mv_matrix", m_mv_mat);
+            }
+            glBindVertexArray(m_vao[i]);
+            glEnable(GL_DEPTH_TEST);
+            glDrawArrays(GL_TRIANGLES, 0, m_vertices[i].size());
+        }
     }
 }
 
@@ -156,15 +172,32 @@ void PlayerRenderer::shadow_render(const Shader& shader,
         model = glm::rotate(model, glm::radians(player.yaw),
                             glm::vec3(0.0f, 1.0f, 0.0f));
 
-        model = glm::translate(model, glm::vec3(-0.5f, 0.0f, -0.5f));
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
 
         shader.set_loc("modelMatrix", model);
-        glBindVertexArray(m_vao);
-        glEnable(GL_DEPTH_TEST);
-        glDrawArrays(GL_TRIANGLES, 0, m_vertices_sum);
+
+        for (int i = 0; i < BODY_PART_NUM; i++) {
+            if (i == 0) {
+                glm::mat4 head_model = model;
+
+                head_model =
+                    glm::translate(head_model, glm::vec3(0.5f, 1.5f, 0.5f));
+
+                head_model =
+                    glm::rotate(head_model, glm::radians(-player.pitch),
+                                glm::vec3(-1, 0, 0));
+
+                head_model =
+                    glm::translate(head_model, glm::vec3(-0.5f, -1.5f, -0.5f));
+                shader.set_loc("modelMatrix", head_model);
+            } else {
+                shader.set_loc("modelMatrix", model);
+            }
+            glBindVertexArray(m_vao[i]);
+            glEnable(GL_DEPTH_TEST);
+            glDrawArrays(GL_TRIANGLES, 0, m_vertices[i].size());
+        }
     }
 }
 
-GLuint PlayerRenderer::vao() { return m_vao; }
-int PlayerRenderer::sum() { return m_vertices_sum; }
 } // namespace Cubed
