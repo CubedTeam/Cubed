ad220bb48cde93115f432f15634554deda2df24f
zhenyan121 <3367366583@qq.com>
2026-07-18 10:39:40 +0800

    feat(debug): enable GL debug context flag in debug builds

--- src/window.cpp
+++ src/window.cpp
@@ -238,6 +238,10 @@ void Window::init(const Argument& argument) {
 
     SDL_SetWindowPosition(m_window, SDL_WINDOWPOS_CENTERED,
                           SDL_WINDOWPOS_CENTERED);
+#ifdef DEBUG_MODE
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+    Logger::debug("Debug Flag On");
+#endif
 }
 
 void Window::reload_config() {
